557b4fe8cace9999c909cfca4d187eeec353e711
jhy <1194345103@qq.com>
2024-05-29 19:19:30 +0800

    update evaluate

--- myevaluate.py
+++ myevaluate.py
@@ -242,6 +242,150 @@ def evaluate_io(args, net, dataset_dict, extended_test=False):
 
         return eval_dict
 
+def evaluate_fixmatch(args, net, dataset_dict, extended_test=False):
+    """
+    evaluation function for open-set SSL setting
+    """
+    # 原始cifar10的测试集作为测试集
+    full_loader = DataLoader(dataset_dict['test']['full'], batch_size=256, drop_last=False, shuffle=False, num_workers=4)
+    if extended_test:  #原始cifar10的测试集加上其他ood数据，其他ood数据有svhn，高斯噪声、均值噪声、（还有一种忘了），每种ood数据量都是一样的
+        extended_loader = DataLoader(dataset_dict['test']['extended'], batch_size=1024, drop_last=False, shuffle=False, num_workers=4)
+
+    total_num = 0.0
+    y_true_list = []#真实标签
+    p_list = []
+    pred_p_list = []
+    pred_hat_q_list = []
+    pred_q_list = []
+    pred_hat_p_list = []
+    ood_scores=[]
+    feat_list=[]
+    with torch.no_grad():
+        for data in tqdm(full_loader):
+            x = data['x_lb']
+            y = data['y_lb']
+
+            if isinstance(x, dict):
+                x = {k: v.cuda() for k, v in x.items()}
+            else:
+                x = x.cuda()  #得到原始此cifar10测试集数据
+            y = y.cuda()     #得到原始ciafar10测试集标签
+            y_true_list.extend(y.cpu().tolist()) #原始测试集的真实标签
+
+            num_batch = y.shape[0]   #batch_size大小
+            total_num += num_batch   #total_num是数据总量大小
+
+            outputs = net(x)
+            logits = outputs['logits']
+            feature=outputs['feat']
+            feat_list.append(feature)
+            # predictions p of closed-set classifier
+            p = F.softmax(logits, 1)
+            pred_p = p.data.max(1)[1]
+            pred_p_list.extend(pred_p.cpu().tolist())
+
+
+        y_true = np.array(y_true_list)
+        closed_mask = y_true < args.num_classes
+        open_mask = y_true >= args.num_classes
+        y_true[open_mask] = args.num_classes#将所有的ood数据标记为args.num_classes，比如6分类任务，inlier标记为1-5，ood标记为6
+
+
+        pred_p = np.array(pred_p_list)  #预测标签列表
+
+        # closed accuracy of p / hat_p on closed test data
+        close_acc = accuracy_score(y_true[closed_mask], pred_p[closed_mask])#在test set中，把inlier拿出来，测试模型对inlier分类的精确度
+        closed_confusion_matrix = confusion_matrix(y_true[closed_mask], pred_p[closed_mask], normalize='true')
+        np.set_printoptions(precision=3, suppress=True)
+
+
+        o_acc_e_q = o_acc_e_hq = 0
+        o_cfmat_e_q = None
+        o_cfmat_e_hq = None
+
+        # auroc
+        # auroc = roc_auc_score(y_true, y_score)#
+        ood_scores = np.array(ood_scores)  # 这里为了方便，ood记为1，id记为0
+        gt_id_or_ood = [0 if val in range(args.num_classes) else 1 for val in y_true_list]
+        gt_id_or_ood = np.array(gt_id_or_ood)
+
+
+        if extended_test:
+            unk_scores = [] # K+1类的概率
+            unk_scores_q = []
+            for data in tqdm(extended_loader):
+                x = data['x_lb']
+                y = data['y_lb']
+
+                if isinstance(x, dict):
+                    x = {k: v.cuda() for k, v in x.items()}
+                else:
+                    x = x.cuda()
+                y = y.cuda()
+                y_true_list.extend(y.cpu().tolist())
+
+                num_batch = y.shape[0]
+                total_num += num_batch
+
+                outputs = net(x)
+                logits = outputs['logits']
+                logits_mb = outputs['logits_mb']
+                logits_open = outputs['logits_open']
+
+                # predictions p of closed-set classifier
+                p = F.softmax(logits, 1)
+                pred_p = p.data.max(1)[1]
+                pred_p_list.extend(pred_p.cpu().tolist())
+
+                # predictions hat_q of (closed-set + multi-binary) classifiers
+                r = F.softmax(logits_mb.view(logits_mb.size(0), 2, -1), 1)
+                tmp_range = torch.arange(0, logits_mb.size(0)).long().cuda()
+                hat_q = torch.zeros((num_batch, args.num_classes + 1)).cuda()
+                o_neg = r[tmp_range, 0, :]
+                o_pos = r[tmp_range, 1, :]
+                unk_score = torch.sum(p * o_neg, 1)
+                unk_scores.extend(unk_score.cpu().tolist())
+                hat_q[:, :args.num_classes] = p * o_pos
+                hat_q[:, args.num_classes] = torch.sum(p * o_neg, 1)
+                pred_hat_q = hat_q.data.max(1)[1]
+                pred_hat_q_list.extend(pred_hat_q.cpu().tolist())
+
+                # predictions q of open-set classifier
+                q = F.softmax(logits_open, 1)
+                pred_q = q.data.max(1)[1]
+                pred_q_list.extend(pred_q.cpu().tolist())
+
+                # prediction hat_p of open-set classifier
+                hat_p = q[:, :args.num_classes] / q[:, :args.num_classes].sum(1).unsqueeze(1)#（不太确定）和上边的方式相比，只是归一化方式不同，上边是softmax归一化，下边是直接除以他们的和进行归一
+                pred_hat_p = hat_p.data.max(1)[1]
+                pred_hat_p_list.extend(pred_hat_p.cpu().tolist())
+
+            y_true = np.array(y_true_list)
+            open_mask = y_true >= args.num_classes
+            y_true[open_mask] = args.num_classes
+
+            pred_q = np.array(pred_q_list)
+            pred_hat_q = np.array(pred_hat_q_list)
+
+            # open accuracy of q / hat_q on extended test data
+            o_acc_e_q = balanced_accuracy_score(y_true, pred_q)
+            o_acc_e_hq = balanced_accuracy_score(y_true, pred_hat_q)
+            o_cfmat_e_q = confusion_matrix(y_true, pred_q, normalize='true')
+            o_cfmat_e_hq = confusion_matrix(y_true, pred_hat_q, normalize='true')
+
+
+        eval_dict = {
+                     'o_acc_e_q': o_acc_e_q, 'o_acc_e_hq': o_acc_e_hq,
+                     'o_cfmat_e_q': o_cfmat_e_q, 'o_cfmat_e_hq': o_cfmat_e_hq,
+                    }
+
+        print(f"#############################################################\n"
+              f" Open Accuracy on Extended Test Data (q / hq): {o_acc_e_q * 100:.2f} / {o_acc_e_hq * 100:.2f}\n"
+              f"#############################################################\n"
+            )
+
+        return eval_dict
+
 #待测的实验设置
 config='config/openset_cv/fixmatch/fixmatch_cifar10_600_0.yaml'
 args = parser.parse_args(args=['--c', config])
@@ -249,7 +393,7 @@ over_write_args_from_file(args, args.c)
 args.data_dir = 'data'
 dataset_dict = get_dataset(args, args.algorithm, args.dataset, args.num_labels, args.num_classes, args.data_dir, eval_open=True)
 best_net = load_model_at('best')
-eval_dict = evaluate_io(args, best_net, dataset_dict)
+eval_dict = evaluate_fixmatch(args, best_net, dataset_dict)
 #默认设置     cifar100_2000   seen/unseen split of 80/20, 25 labels per seen class
 
 
@@ -266,8 +410,8 @@ plt.show()
 
 
 # Confusion matrix of open-set classification
-fig = plt.figure()
-f, ax = plt.subplots(figsize=(12,10))
-cf_mat = eval_dict['o_cfmat_f_q']
-ax = sns.heatmap(cf_mat, cmap='YlGn', linewidth=0.5)
-plt.show()
+# fig = plt.figure()
+# f, ax = plt.subplots(figsize=(12,10))
+# cf_mat = eval_dict['o_cfmat_f_q']
+# ax = sns.heatmap(cf_mat, cmap='YlGn', linewidth=0.5)
+# plt.show()
