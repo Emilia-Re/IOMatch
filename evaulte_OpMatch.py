7252782a937925306f78f9cd4d610236d22a153a
jhy <1194345103@qq.com>
2024-07-03 11:45:31 +0800

    update

--- evaulte_OpMatch.py
+++ evaulte_OpMatch.py
@@ -4,7 +4,7 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--c', type=str, default='')
     #OpenMatch
-    args = parser.parse_args(args=['--c', 'config/openset_cv/openmatch/openmatch_cifar10_150_0.yaml'])
+    args = parser.parse_args(args=['--c', 'config/openset_cv/openmatch/openmatch_cifar10_300_0.yaml'])
     over_write_args_from_file(args, args.c)
     args.img_save_dir=os.path.join(args.save_dir,args.save_name)
     args.data_dir = 'data'
